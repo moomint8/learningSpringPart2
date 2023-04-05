959ad536186c88fe14bc821cc193134ebe26fc49
moomint8 <office05053@gmail.com>
2023-04-05 16:27:13 +0900

    23/04/05 16:25 스프링 핵심 원리 이해1 예제 만들기 - 회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();   // 실제로는 동시성 이슈 때문에 컨커런트 해시맵을 사용해야 함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
