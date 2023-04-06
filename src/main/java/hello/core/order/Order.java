57d114926ccf03d18c8a3bffa8125d84700e7d8e
moomint8 <office05053@gmail.com>
2023-04-06 17:30:10 +0900

    23/04/06 17:30 스프링 핵심 원리 이해1 예제 만들기 - 회원 도메인 실행과 테스트 ~ 주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,50 @@
+package hello.core.order;
+
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculatePrice() {
+        if(itemPrice >= discountPrice) {    // 가격 > 할인금액
+            return itemPrice - discountPrice;
+        } else {
+            return 0;
+        }
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
